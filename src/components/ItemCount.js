a93fdde8ee95129c7e02a9c39d7856cb56604ae7
Santiago Valdez <santiagodval@outlook.com>
2022-09-19 18:36:21 -0300

    asd

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -5,13 +5,21 @@ function ItemCount({stock, initial, onAdd}){
 
     let[counter, setCounter] = useState(initial)
 
+    const decrementar = ()=>{
+        setCounter(counter>1 ? counter-1 : counter);
+    }
+
+    const aumentar = () =>{
+        setCounter(counter<stock ? counter+1 : counter)
+    }
+
     return(
         <div className="contador">
             <h4>Camisa de tigre</h4>
-            <button id="subtract" onClick={() => setCounter(counter>1 ? counter-1 : counter)}>-</button>
+            <button id="subtract" onClick={decrementar}>-</button>
             <div className="counter">{counter}</div>
-            <button id="add" onClick={()=>setCounter(counter<stock ? counter+1 : counter)}>+</button>
-            <button className="addButton">Agregar al carrito</button>
+            <button id="add" onClick={aumentar}>+</button>
+            <button className="addButton" onClick={()=>onAdd(counter)}>Agregar al carrito</button>
         </div>
     )
 };
--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -1,5 +1,14 @@
+import ItemCount from "../ItemCount";
+import { useState } from "react";
 
 function ItemDetail( {item} ) {
+
+    const [contador, setContador] = useState(0);
+
+    const onAdd= (counter) =>{
+        console.log(counter);
+        setContador(counter);
+    }
 
     return (
         <div className="item">
@@ -7,6 +16,9 @@ function ItemDetail( {item} ) {
             <img width={100} height={100} src={item.pictureUrl}></img>
             <p className="precio">Precio: {item.price}</p>
             <p className="stock">Stock disponible: {item.stock}</p>
+            <ItemCount stock={10} initial={1} onAdd={onAdd}/>
+            <h5>{contador}</h5>
+
         </div>
     );
 };
